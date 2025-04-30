133926dbe695477b93c83ff32f9671aec3b5c276
aadil <aadil@inbox-infotech.com>
2025-04-30 18:33:56 +0530

    update contact us fomr

--- app/Http/Controllers/Front/FrontController.php
+++ app/Http/Controllers/Front/FrontController.php
@@ -156,11 +156,11 @@ class FrontController extends Controller
             'ready_to_reallocates' => 'required',
         ]);
 
-        
+
         $arr = new Jobapplication();
         $message = "Thanks for apply, We have received your application!";
         $arr->status = 1;
-        
+
         if ($req->hasfile('resume')) {
             $image = $req->file('resume');
             $ext = $image->extension();
@@ -168,20 +168,20 @@ class FrontController extends Controller
             $image->storeAs('/public/media', $image_name);
             $arr->resume = $image_name;
         }
-        
+
         $arr->job_post_id = $req->jobid;
         $arr->first_name = $req->first_name;
         $arr->last_name = $req->last_name;
-        
+
         $arr->address = $req->address;
-        
+
         $arr->city = $req->city;
         $arr->state = $req->state;
         $arr->ready_to_reallocates = $req->ready_to_reallocates;
         $arr->email_id = $req->email_id;
         $arr->phone_no = $req->phone_no;
         // $arr->save();    
-        
+
         $fn = $req->first_name;
         $ln = $req->last_name;
         $fullname =  $fn . ' ' . $ln;
@@ -214,6 +214,9 @@ class FrontController extends Controller
             'email' => 'required|email|unique:contactus,email',
         ]);
 
+        // Combine country code with phone number
+        $fullPhone = $request->country_code . $request->phone;
+
         $data = new Contactu();
         $data->created_at = Carbon::now()->toDateTimeString();
         $data->updated_at = Carbon::now()->toDateTimeString();
@@ -221,25 +224,24 @@ class FrontController extends Controller
         $data->message = $request->message;
         $data->name = $request->name;
         $data->email = $request->email;
+        $data->phone = $fullPhone; // Save combined phone
         $data->status = 0;
         $data->save();
 
+        // Spam word filtering
         $string = strtolower($request->name . $request->message);
+        $spamwords = ['women sex', 'sex', 'best women', 'porn', 'viagra', 'girl', 'sexy girl', 'hot girl', 'girls', 'hot sexy'];
 
-        $spamwords = array('women sex', 'sex', 'best women', 'porn', 'viagra', 'girl', 'sexy girl', 'hot girl', 'girls', 'hot sexy');
-
-        foreach ($spamwords as $url) {
-            if (strpos($string, $url) !== FALSE) {
+        foreach ($spamwords as $word) {
+            if (strpos($string, $word) !== false) {
                 return back();
-                return false;
-                die;
             }
         }
 
-        $fullname =  $request->name;
+        $fullname = $request->name;
         $emails = "info@inboxtechs.com";
         $supportno = $request->email;
-        $data = array('name' => $fullname, 'supportno' => $supportno, 'details' => $request->message);
+        $data = ['name' => $fullname, 'supportno' => $supportno, 'details' => $request->message];
         $subject = "Request Call Back / Inquiry";
 
         Mail::send('email.contactus', $data, function ($message) use ($emails, $subject) {
@@ -248,6 +250,7 @@ class FrontController extends Controller
 
         return redirect('/contact-us')->with('success', "Thank You, We have received your response");
     }
+
     public function industries()
     {
         $arr['industries'] = DB::table('industries')->where('status', 1)->WhereNull('deleted_at')->get();
